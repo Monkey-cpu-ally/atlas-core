3d82df032690325fc32eeed352272d2c4c637e69
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-02-17 10:07:46 -0600

    Add atlas_core_new/core/personas/__init__.py

---
+++ atlas_core_new/core/personas/__init__.py
@@ -0,0 +1,5 @@
+"""atlas_core/core/personas - Persona definitions and registry."""
+from .definitions import PersonaDef, AJANI, MINERVA, HERMES
+from .registry import PersonaRegistry
+
+__all__ = ["PersonaDef", "AJANI", "MINERVA", "HERMES", "PersonaRegistry"]
