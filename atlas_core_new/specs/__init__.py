bf344d9ece17b95c721e7c398e30e5b1a418525d
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-02-17 10:09:14 -0600

    Add atlas_core_new/specs/__init__.py

---
+++ atlas_core_new/specs/__init__.py
@@ -0,0 +1,34 @@
+"""
+ATLAS-PRIME Technical & Functional Specifications
+
+Complete system specs for the AI council - what they are, what they do,
+and what they are NOT.
+"""
+from .architecture import CORE_ARCHITECTURE, TRI_CORE_COUNCIL
+from .intelligence import COGNITIVE_CAPABILITIES, LEARNING_INTELLIGENCE
+from .teaching import TEACHING_SYSTEM, ASSESSMENT_SYSTEM
+from .autonomy import AUTONOMY_SPECS, SYSTEM_BOUNDARIES
+from .personality import PERSONALITY_SPECS, BEHAVIORAL_TRAITS
+from .capabilities import MULTIMODAL_CAPABILITIES, HARDWARE_AWARENESS
+from .security import SECURITY_SPECS, ETHICS_LAYER
+from .knowledge_spectrum import KNOWLEDGE_SPECTRUM
+from .evolution import EVOLUTION_SPECS
+
+__all__ = [
+    'CORE_ARCHITECTURE',
+    'TRI_CORE_COUNCIL',
+    'COGNITIVE_CAPABILITIES',
+    'LEARNING_INTELLIGENCE',
+    'TEACHING_SYSTEM',
+    'ASSESSMENT_SYSTEM',
+    'AUTONOMY_SPECS',
+    'SYSTEM_BOUNDARIES',
+    'PERSONALITY_SPECS',
+    'BEHAVIORAL_TRAITS',
+    'MULTIMODAL_CAPABILITIES',
+    'HARDWARE_AWARENESS',
+    'SECURITY_SPECS',
+    'ETHICS_LAYER',
+    'KNOWLEDGE_SPECTRUM',
+    'EVOLUTION_SPECS'
+]
