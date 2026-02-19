8449d707cbdd20987e9dc424a69a4ce22a40825f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-19 04:02:30 +0000

    FlutterFlow export

---
+++ lib/app_state.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FFAppState extends ChangeNotifier {
+  static FFAppState _instance = FFAppState._internal();
+
+  factory FFAppState() {
+    return _instance;
+  }
+
+  FFAppState._internal();
+
+  static void reset() {
+    _instance = FFAppState._internal();
+  }
+
+  Future initializePersistedState() async {
+    prefs = await SharedPreferences.getInstance();
+    _safeInit(() {
+      _aiMode = prefs.getString('ff_aiMode') ?? _aiMode;
+    });
+  }
+
+  void update(VoidCallback callback) {
+    callback();
+    notifyListeners();
+  }
+
+  late SharedPreferences prefs;
+
+  /// Control the AI face behavior state: idle,listening, thinking,speaking
+  String _aiMode = '';
+  String get aiMode => _aiMode;
+  set aiMode(String value) {
+    _aiMode = value;
+    prefs.setString('ff_aiMode', value);
+  }
+
+  /// True while AI is processing a response
+  bool _isThinking = false;
+  bool get isThinking => _isThinking;
+  set isThinking(bool value) {
+    _isThinking = value;
+  }
+
+  /// True while AI is speaking or outputting text/audio
+  bool _isSpeaking = false;
+  bool get isSpeaking => _isSpeaking;
+  set isSpeaking(bool value) {
+    _isSpeaking = value;
+  }
+
+  /// True when microphone is actively listening
+  bool _isListening = false;
+  bool get isListening => _isListening;
+  set isListening(bool value) {
+    _isListening = value;
+  }
+}
+
+void _safeInit(Function() initializeField) {
+  try {
+    initializeField();
+  } catch (_) {}
+}
+
+Future _safeInitAsync(Function() initializeField) async {
+  try {
+    await initializeField();
+  } catch (_) {}
+}
