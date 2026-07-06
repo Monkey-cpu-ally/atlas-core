7c6cd6b61154734cafcaf4b3ef5f918b7ee184e9
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-06 11:03:39 -0500

    Add Creative Intelligence package exports

---
+++ creative_intelligence/__init__.py
@@ -0,0 +1,6 @@
+"""ATLAS Creative Intelligence Division."""
+
+from .creative_engine import CreativeIntelligenceEngine
+from .schemas import CreativeBrief, CreativePlan
+
+__all__ = ["CreativeIntelligenceEngine", "CreativeBrief", "CreativePlan"]
