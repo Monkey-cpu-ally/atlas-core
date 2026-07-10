1c391d703347a41b02bcc9a83e177d894c69a43f
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-10 11:53:26 -0500

    Export Animation Studio design engines

--- atlas_animation_studio/__init__.py
+++ atlas_animation_studio/__init__.py
@@ -1,5 +1,23 @@
 """ATLAS Animation Studio package."""
 
+from .character_design_engine import CharacterDesignEngine, CharacterDesignPlan
+from .color_lighting_engine import ColorLightingEngine, ColorLightingPlan
+from .concept_art_engine import ConceptArtBrief, ConceptArtEngine
+from .creature_design_engine import CreatureDesignEngine, CreatureDesignPlan
+from .environment_design_engine import EnvironmentDesignEngine, EnvironmentDesignPlan
 from .storyboard_engine import StoryboardEngine, StoryboardFrame
 
-__all__ = ["StoryboardEngine", "StoryboardFrame"]
+__all__ = [
+    "CharacterDesignEngine",
+    "CharacterDesignPlan",
+    "ColorLightingEngine",
+    "ColorLightingPlan",
+    "ConceptArtBrief",
+    "ConceptArtEngine",
+    "CreatureDesignEngine",
+    "CreatureDesignPlan",
+    "EnvironmentDesignEngine",
+    "EnvironmentDesignPlan",
+    "StoryboardEngine",
+    "StoryboardFrame",
+]
