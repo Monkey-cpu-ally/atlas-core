7437d83c8798b485bcf8ec54149a0aa653f97cf2
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-13 01:58:32 -0500

    Wire React HUD into ATLAS visual bridge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import "@/App.css";
 import HUDInterface from "./components/HUDInterface";
+import useAtlasVisualBridge from "./hooks/useAtlasVisualBridge";
 
 function App() {
+  const visualBridge = useAtlasVisualBridge();
+
   return (
-    <div className="App">
-      <HUDInterface />
+    <div
+      className="App"
+      data-atlas-visual-status={visualBridge.status}
+      data-atlas-last-event={visualBridge.lastEvent?.event || "none"}
+    >
+      <HUDInterface visualBridge={visualBridge} />
     </div>
   );
 }
