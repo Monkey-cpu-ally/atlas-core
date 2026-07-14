d4e7e4b82f040bca9b5c7a42feed4811fece5490
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-14 00:22:21 -0500

    Add feature-flagged ATLAS Genesis Hub

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import "@/App.css";
 import HUDInterface from "./components/HUDInterface";
+import GenesisHub from "./genesis/GenesisHub";
 import useAtlasVisualBridge from "./hooks/useAtlasVisualBridge";
 
 function App() {
   const visualBridge = useAtlasVisualBridge();
+  const genesisEnabled = process.env.REACT_APP_ATLAS_GENESIS_HUB === "true";
 
   return (
     <div
@@ -11,7 +13,11 @@ function App() {
       data-atlas-visual-status={visualBridge.status}
       data-atlas-last-event={visualBridge.lastEvent?.event || "none"}
     >
-      <HUDInterface visualBridge={visualBridge} />
+      {genesisEnabled ? (
+        <GenesisHub visualBridge={visualBridge} />
+      ) : (
+        <HUDInterface visualBridge={visualBridge} />
+      )}
     </div>
   );
 }
