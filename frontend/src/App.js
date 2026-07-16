1acf4875b5d17f01b6cca1a33c71bb33f5f4f581
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-16 07:13:00 -0500

    feat(workspaces): mount Hermes workspace controller

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "@/App.css";
 import "@/hud-clean-mode.css";
 import HUDInterface from "./components/HUDInterface";
 import CoreChatBridge from "./components/HUD/CoreChatBridge";
+import WorkspaceController from "./components/workspaces/WorkspaceController";
 
 function App() {
   return (
     <div className="App">
       <HUDInterface />
       <CoreChatBridge />
+      <WorkspaceController />
     </div>
   );
 }
