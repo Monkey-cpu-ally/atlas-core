f30508e4b7ec98d7f128d215a69959fadfc29ff7
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-16 22:01:07 -0500

    fix(hud): keep interface alive when a panel crashes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import "@/App.css";
 import "@/hud-clean-mode.css";
 import HUDInterface from "./components/HUDInterface";
 import CoreChatBridge from "./components/HUD/CoreChatBridge";
+import HUDErrorBoundary from "./components/HUD/HUDErrorBoundary";
 import WorkspaceController from "./components/workspaces/WorkspaceController";
 import HermesPresenceBridge from "./components/workspaces/HermesPresenceBridge";
 
 function App() {
   return (
     <div className="App">
-      <HUDInterface />
-      <CoreChatBridge />
-      <HermesPresenceBridge />
-      <WorkspaceController />
+      <HUDErrorBoundary>
+        <HUDInterface />
+        <CoreChatBridge />
+        <HermesPresenceBridge />
+        <WorkspaceController />
+      </HUDErrorBoundary>
     </div>
   );
 }
