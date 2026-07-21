e57655cf2199c3a89222eda2077d3e68d7bddb99
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-21 07:28:59 -0500

    Export luxury workflow manufacturing engineering and evaluation services

--- atlas_core/luxury/__init__.py
+++ atlas_core/luxury/__init__.py
@@ -1,8 +1,25 @@
 from .council import CouncilReviewEngine
 from .critique import LuxuryCritiqueEngine, OriginalityEngine
 from .database import LuxuryDatabase
+from .engineering import (
+    ApparelEngineeringCalculator,
+    ApparelEngineeringResult,
+    BagEngineeringCalculator,
+    BagEngineeringResult,
+    FootwearEngineeringCalculator,
+    FootwearEngineeringResult,
+    FurnitureEngineeringCalculator,
+    FurnitureEngineeringResult,
+)
+from .evaluation_store import EvaluationStore
 from .forge import DesignForge
 from .genome import DEFAULT_CATEGORIES, DesignGenomeEngine
+from .manufacturing import (
+    CostLine,
+    ManufacturingCostEngine,
+    ManufacturingEstimate,
+    ManufacturingInputs,
+)
 from .models import (
     AIReview,
     CouncilDecision,
@@ -18,12 +35,18 @@ from .models import (
 from .progress import AcademyProgressTracker, ProgressModule, ProgressRepository
 from .repositories import DesignProjectRepository, MaterialRepository
 from .service import LuxuryDesignService
+from .workflow import ForgeStateMachine, ForgeTransitionError, StageRequirement
 
 __all__ = [
     "AIReview",
     "AcademyProgressTracker",
+    "ApparelEngineeringCalculator",
+    "ApparelEngineeringResult",
+    "BagEngineeringCalculator",
+    "BagEngineeringResult",
     "CouncilDecision",
     "CouncilReviewEngine",
+    "CostLine",
     "CritiqueFinding",
     "DEFAULT_CATEGORIES",
     "DesignConcept",
@@ -31,16 +54,27 @@ __all__ = [
     "DesignGenome",
     "DesignGenomeEngine",
     "DesignProjectRepository",
+    "EvaluationStore",
+    "FootwearEngineeringCalculator",
+    "FootwearEngineeringResult",
     "ForgeRecord",
     "ForgeStage",
+    "ForgeStateMachine",
+    "ForgeTransitionError",
+    "FurnitureEngineeringCalculator",
+    "FurnitureEngineeringResult",
     "GenomeScore",
     "LuxuryCritiqueEngine",
     "LuxuryDatabase",
     "LuxuryDesignService",
+    "ManufacturingCostEngine",
+    "ManufacturingEstimate",
+    "ManufacturingInputs",
     "MaterialProfile",
     "MaterialRepository",
     "OriginalityEngine",
     "ProgressModule",
     "ProgressRepository",
     "ReviewVerdict",
+    "StageRequirement",
 ]
