08237924e35e360f7c0a30aa99e2d671e216e52a
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-07-20 23:48:16 -0500

    Export persistence and progress services

--- atlas_core/luxury/__init__.py
+++ atlas_core/luxury/__init__.py
@@ -1,5 +1,6 @@
 from .council import CouncilReviewEngine
 from .critique import LuxuryCritiqueEngine, OriginalityEngine
+from .database import LuxuryDatabase
 from .forge import DesignForge
 from .genome import DEFAULT_CATEGORIES, DesignGenomeEngine
 from .models import (
@@ -14,9 +15,13 @@ from .models import (
     MaterialProfile,
     ReviewVerdict,
 )
+from .progress import AcademyProgressTracker, ProgressModule, ProgressRepository
+from .repositories import DesignProjectRepository, MaterialRepository
+from .service import LuxuryDesignService
 
 __all__ = [
     "AIReview",
+    "AcademyProgressTracker",
     "CouncilDecision",
     "CouncilReviewEngine",
     "CritiqueFinding",
@@ -25,11 +30,17 @@ __all__ = [
     "DesignForge",
     "DesignGenome",
     "DesignGenomeEngine",
+    "DesignProjectRepository",
     "ForgeRecord",
     "ForgeStage",
     "GenomeScore",
     "LuxuryCritiqueEngine",
+    "LuxuryDatabase",
+    "LuxuryDesignService",
     "MaterialProfile",
+    "MaterialRepository",
     "OriginalityEngine",
+    "ProgressModule",
+    "ProgressRepository",
     "ReviewVerdict",
 ]
