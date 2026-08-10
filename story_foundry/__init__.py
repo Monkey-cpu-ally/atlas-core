14c7287e56cdf1a6a45ceb6a696acae2d11c7642
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-08-10 18:12:43 -0500

    Export Story Foundry bible engines

--- story_foundry/__init__.py
+++ story_foundry/__init__.py
@@ -1,5 +1,14 @@
 """ATLAS Story Foundry package."""
 
+from .character_bible_engine import CharacterBible, CharacterBibleEngine
 from .story_foundry_engine import StoryFoundryEngine, StorySeed
+from .world_bible_engine import WorldBible, WorldBibleEngine
 
-__all__ = ["StoryFoundryEngine", "StorySeed"]
+__all__ = [
+    "CharacterBible",
+    "CharacterBibleEngine",
+    "StoryFoundryEngine",
+    "StorySeed",
+    "WorldBible",
+    "WorldBibleEngine",
+]
