a1b420ebf164af80ddca082e4ae050ad12d917bd
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-08-10 18:12:51 -0500

    Export Council Review engine

--- creative_intelligence/__init__.py
+++ creative_intelligence/__init__.py
@@ -1,6 +1,13 @@
 """ATLAS Creative Intelligence Division."""
 
+from .council_review_engine import CouncilReview, CouncilReviewEngine
 from .creative_engine import CreativeIntelligenceEngine
 from .schemas import CreativeBrief, CreativePlan
 
-__all__ = ["CreativeIntelligenceEngine", "CreativeBrief", "CreativePlan"]
+__all__ = [
+    "CouncilReview",
+    "CouncilReviewEngine",
+    "CreativeIntelligenceEngine",
+    "CreativeBrief",
+    "CreativePlan",
+]
