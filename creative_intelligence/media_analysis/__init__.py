42e64e1901a288fe3afdbd6de4d8a3451494807d
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-08-14 10:07:42 -0500

    Export media adapters and memory bridge

--- creative_intelligence/media_analysis/__init__.py
+++ creative_intelligence/media_analysis/__init__.py
@@ -1,11 +1,27 @@
 """Reference media analysis package for ATLAS."""
 
+from .adapters import (
+    MediaInput,
+    ObservationAdapter,
+    StoryObservationAdapter,
+    VideoObservationAdapter,
+    VideoProvider,
+    VisionProvider,
+)
+from .memory_bridge import MediaStudyMemoryBridge
 from .pipeline import ReferenceMediaAnalyzer
 from .schemas import MediaStudyReport, StoryObservation, VisualObservation
 from .story_analyzer import StoryStructureAnalyzer
 from .visual_analyzer import VisualCraftAnalyzer
 
 __all__ = [
+    "MediaInput",
+    "ObservationAdapter",
+    "StoryObservationAdapter",
+    "VideoObservationAdapter",
+    "VideoProvider",
+    "VisionProvider",
+    "MediaStudyMemoryBridge",
     "ReferenceMediaAnalyzer",
     "MediaStudyReport",
     "StoryObservation",
