df646ca18fda90026ca4a6cd2c1aa8a1a9f11f67
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-08-15 10:04:03 -0500

    Expose Creative Production Orchestrator

---
+++ creative_production/__init__.py
@@ -0,0 +1,13 @@
+"""ATLAS Creative Production pipeline."""
+
+from .orchestrator import (
+    CreativeProductionOrchestrator,
+    CreativeProductionPackage,
+    CreativeProductionRequest,
+)
+
+__all__ = [
+    "CreativeProductionOrchestrator",
+    "CreativeProductionPackage",
+    "CreativeProductionRequest",
+]
