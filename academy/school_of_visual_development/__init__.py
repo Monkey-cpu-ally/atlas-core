e96c82f0b799eae5dfcece09376f8492f318fa49
Monkey-cpu-ally <fdeondrae21@gmail.com>
2026-08-14 23:42:51 -0500

    Export visual development engine

---
+++ academy/school_of_visual_development/__init__.py
@@ -0,0 +1,5 @@
+"""ATLAS School of Visual Development."""
+
+from .visual_development_engine import VisualDevelopmentEngine, VisualDevelopmentPlan
+
+__all__ = ["VisualDevelopmentEngine", "VisualDevelopmentPlan"]
